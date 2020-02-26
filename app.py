754834240aacb362c7ccd1e1248a6b6b8a59ccda
Antoine Rico <antoinerico1@gmail.com>
2020-02-26 20:10:39 +0100

    qs

--- app.py
+++ app.py
@@ -1,107 +1,75 @@
-Eimport RPi.GPIO as GPIO
-import pygame
-import time
-import curses
+import os
+import RPi.GPIO as GPIO
+import pygame, time
+from random import *
+from enum import Enum
 from flask import Flask, render_template
+
+
+
+
+
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index_BP_moine.html')
+# this function plays some random animals noise when the webpage button
+	return render_template('index.html')
 
-@app.route('/bonjour/<name>')
-def hello(name):
-    return render_template('page_nom.html', name=name)
 
 @app.route('/moine')
 def moine():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(21,GPIO.IN) #rondelle verte
-
-    pygame.mixer.pre_init(44100, -16, 2, 512)
+# this function plays some random animals noise when the webpage button
+	randa=randint(0,len(fil)-1)
+	pygame.mixer.music.load(PlaylistDrumset + fil[randa])
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() > 0:
+		time.sleep(1)
+	return render_template('index.html')
+	
+	
+@app.route('/start')
+def start():
     pygame.init()
+    pygame.mixer.init() 
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(21,GPIO.IN) 
+    #rondelle verte
+    #GPIO.add_even_detect(21,GPIO.BOTH, callback=light_callback, bouncetime=75)   
+    if GPIO.input(21)==0:
+        print("coucou toua")
+        light_callback()
+    return render_template('index.html')
 
-    ## Init
-    ## Drumset index
-    i = 0
-    k = 0
-    currentDrumset= '/home/pi/Desktop/a/rico'
-    p = pygame.mixer.Sound(currentDrumset + '/Verg2.wav')    
-    m = pygame.mixer.Sound(currentDrumset + '/Verg1.wav')
-    z = pygame.mixer.Sound(currentDrumset + '/1.wav')
-    e = pygame.mixer.Sound(currentDrumset + '/2.wav')    
-    d = pygame.mixer.Sound(currentDrumset + '/3.wav')
-    f = pygame.mixer.Sound(currentDrumset + '/5.wav')
-    g = pygame.mixer.Sound(currentDrumset + '/4.wav')
-    ## Screen display$
-    print("ok avant test lum")
-    while(k==0):
-        while(i==0):
-            ##print("ok dans la boucle while")
-            if GPIO.input(21)==0:
-                ## Update the song folder to the selected drum set
-                print("m vaut moine")
-                m.play()
-                time.sleep(26)
-                i=1;
-        i=0;
-        while(i==0):
-            ##print("ok dans la boucle while")
-            if GPIO.input(21)==0:
-                ## Update the song folder to the selected drum set
-                print("z vaut 1")
-                z.play()
-                time.sleep(46)
-                i=1;
-        i=0;
-        while(i==0):
-            ##print("ok dans la boucle while")
-            if GPIO.input(21)==0:
-                ## Update the song folder to the selected drum set
-                print("e vaut 2")
-                e.play()
-                time.sleep(46)
-                i=1;
-        i=0;
-        while(i==0):
-            ##print("ok dans la boucle while")
-            if GPIO.input(21)==0:
-                ## Update the song folder to the selected drum set
-                print("d vaut 3")
-                d.play()
-                time.sleep(46)
-                i=1;
-                k=1;
-        while(i==0):
-            ##print("ok dans la boucle while")
-            if GPIO.input(21)==0:
-                ## Update the song folder to the selected drum set
-                print("f vaut 5")
-                f.play()
-                time.sleep(52)
-                i=1;
-                k=1;
-        while(i==0):
-            ##print("ok dans la boucle while")
-            if GPIO.input(21)==0:
-                ## Update the song folder to the selected drum set
-                print("g vaut 4")
-                g.play()
-                time.sleep(52)
-                i=1;
-                k=1;
-    while(k==0):
-        while(i==0):
-            ##print("ok dans la boucle while")
-            if GPIO.input(21)==0:
-                ## Update the song folder to the selected drum set
-                print("p vaut le doigt")
-                p.play()
-                time.sleep(57)
-                i=1;
-        i=0;
-    return render_template('index_BP_home.html')
+
+@app.route('/stop')
+def stop():
+    #GPIO.remove_even_detect(21)
+    # Il connait pas cette commande
+    pygame.quit()
+    return render_template('index.html')
+
+def liste_fichier_repertoire():
+    fil = []
+    fil = os.listdir("/home/pi/Desktop/Playlist/List")    
+    return fil
+
+def light_callback():
+    if GPIO.input(21)==0:
+        print("go moine")
+        moine()
+    else:
+        pygame.mixer.music.stop()
 
 if __name__ == '__main__':
+    PlaylistDrumset= '/home/pi/Desktop/Playlist/List/'	
+    fil = []
+    fil = liste_fichier_repertoire()
+    for i, f in enumerate(fil):
+        print("fichier ", f)
+        if i > 50:
+            break
     app.run(debug=True, host='0.0.0.0')
